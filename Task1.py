31f2711bf7158b61ba6cf3365d2f4a4db3ce2e07
PriyanshutheGenius <pk02111622@gmail.com>
2025-09-21 04:37:04 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,29 @@
+"""                        ASSIGNMENT 1:
+                      Module 2: Basic Python Concepts
+                 Task 1: Perform Basic Mathematical Operations
+
+           Problem Statement: Write a Python program that does the following:
+
+1. Takes two numbers as input from the user.
+"""
+
+n1=float(input('Enter the first number: '))
+n2=float(input('Enter the second number: '))
+
+#2. Performs the basic mathematical operations on these two numbers:
+
+# Addition
+
+print("Addition:", n1+n2)
+
+#Subtraction
+
+print("Subtraction:", n1-n2)
+
+#Multiplication
+
+print("Multiplication:", n1*n2)
+
+#Division
+
+print("Division:", n1/n2)

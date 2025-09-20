faf00caeb784db975f627d9773fec8f24fcd9730
PriyanshutheGenius <pk02111622@gmail.com>
2025-09-21 04:19:36 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,38 @@
+"""                        ASSIGNMENT 1:
+                      Module 2: Basic Python Concepts
+                 Task 1: Perform Basic Mathematical Operations
+
+           Problem Statement: Write a Python program that does the following:
+
+1. Takes two numbers as input from the user.
+"""
+
+n1: str=input('Enter the first number: ')
+n2=input('Enter the second number: ')
+
+#2. Performs the basic mathematical operations on these two numbers:
+
+# Addition
+
+a=16
+b=30
+print(a+b)
+
+#Subtraction
+
+c=80
+d=35
+print(c-d)
+
+#Multiplication
+
+e=18
+f=95
+print(e*f)
+
+#Division
+
+g=38
+h=2
+print(g/h)
+
---
+++ Task2.py
@@ -0,0 +1,16 @@
+"""                          Task 2: Create a Personalized Greeting
+
+Problem Statement: Write a Python program that does the following:
+"""
+   #1. Takes a user's first name and last name as input.
+
+m1=input('Enter your First name:')
+m2=input('Enter your last name: ')
+
+#2. Concatenates the first name and last name into a full name.
+
+full_name=m1+ " " +m2
+
+#3. Prints a personalized greeting message using the full name.
+
+print('Hello,', full_name,  'Welcome to the Python program')
